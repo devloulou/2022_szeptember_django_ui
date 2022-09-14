2aa3b43874da02c7382eb502b30dc6e0b40af957
Richard Kovacs <dev.kovacs.richard90@gmail.com>
2022-09-14 20:50:17 +0200

    template structure iplement

--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -4,9 +4,33 @@ from django.http import HttpResponse
 """
 MVC - MVT: Model View Template
 """
+posts = [
+    {
+        'author': 'Ricsi Kovács',
+        'title': 'Blog post 1',
+        'content': 'First content',
+        'date_posted': '2022 szeptember 14'
+    },
+    {
+        'author': 'Ricsi Kovács',
+        'title': 'Blog post 2',
+        'content': 'Second content',
+        'date_posted': '2022 szeptember 15'
+    },
+    {
+        'author': 'Ricsi Kovács',
+        'title': 'Blog post 3',
+        'content': '3rd content',
+        'date_posted': '2022 szeptember 17'
+    }
+]
+
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context=context)
 
 def about(request):
     return render(request, 'blog/about.html')
