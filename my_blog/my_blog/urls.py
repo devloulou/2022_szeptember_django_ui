5be6545ee199a86198b7aeb372f0d07191fa108c
Richard Kovacs <dev.kovacs.richard90@gmail.com>
2022-10-05 17:19:01 +0200

    implement registration

--- my_blog/my_blog/urls.py
+++ my_blog/my_blog/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from user_registration.views import register as register_view
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('admin/', admin.site.urls),
+    path('register/', register_view, name="register"),
+    path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name="login"),
 ]
 
 
